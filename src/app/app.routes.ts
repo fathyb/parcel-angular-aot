046cac477bbfabdf3b98ea28a42e1d8679a1337b
Fathy Boundjadj <fathy.boundjadj@gmail.com>
2017-12-14 23:53:28 +0100

    feat: partial lazy-loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,28 @@ import { HomeComponent } from './shared/components/home/home.component';
 import { LazyComponent } from './shared/components/lazy/lazy.component';
 const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'lazy', component: LazyComponent }
+    { path: 'lazy', loadChildren: './shared/components/lazy/lazy.module#LazyModule' }
 ];
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
+
+const ctx = window as any
+
+ctx.System = {
+  async import() {
+    await import('./shared/components/lazy/lazy.module.ngfactory')
+    const req = ctx['req' + 'u' + 'ire']
+
+    const mod = Object
+      .keys(req.cache)
+      .map(key => req.cache[key])
+      .find(mod => {
+        console.log('mode', mod)
+
+        return mod.exports && mod.exports.LazyModuleNgFactory
+      })
+
+    console.log('factory', mod)
+
+    return mod.exports
+  }
+}
