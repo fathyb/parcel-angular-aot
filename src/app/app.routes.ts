90de1a95fc9abecfc20063e5b08661f8c357251d
Fathy Boundjadj <fathy.boundjadj@ubleam.com>
2017-12-31 09:05:25 +0100

    chore: use new plugins

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,9 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import {HomeComponent} from './shared/components/home/home.component'
-import {LazyComponent} from './shared/components/lazy/lazy.component'
-
 const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'lazy', component: LazyComponent }
+    { path: '', loadChildren: './shared/components/home/home.module#HomeModule' },
+    { path: 'lazy', loadChildren: './shared/components/lazy/lazy.module#LazyModule' }
 ];
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
 
